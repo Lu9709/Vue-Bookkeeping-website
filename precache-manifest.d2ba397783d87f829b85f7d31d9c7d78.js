a2b870e0469a260ccff8f51b1739b75f18ef924f
ASUS-PC <919041098@qq.com>
2021-04-19 09:51:46 +0800

    deploy

---
+++ precache-manifest.d2ba397783d87f829b85f7d31d9c7d78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491067c520727ebff14f",
+    "url": "/Vue-Bookkeeping-website/css/app.2f3fb713.css"
+  },
+  {
+    "revision": "a3b8b195d07d2cbce9f07181767aad71",
+    "url": "/Vue-Bookkeeping-website/index.html"
+  },
+  {
+    "revision": "491067c520727ebff14f",
+    "url": "/Vue-Bookkeeping-website/js/app.b572343c.js"
+  },
+  {
+    "revision": "3a311ce0277dd7871158",
+    "url": "/Vue-Bookkeeping-website/js/chunk-vendors.ac917615.js"
+  },
+  {
+    "revision": "12fba6db59622893a20ad82983445e30",
+    "url": "/Vue-Bookkeeping-website/manifest.json"
+  },
+  {
+    "revision": "e401427448d1dff9931db7c00352edbd",
+    "url": "/Vue-Bookkeeping-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Vue-Bookkeeping-website/robots.txt"
+  }
+]);
